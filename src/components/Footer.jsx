0597415537a8d60ff4abec7b6631f8125879e3ad
Artem Bobylev <artematrr@gmail.com>
2024-02-19 14:58:25 +0400

    update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,14 @@
 function Footer() {
-	return <footer>Footer</footer>
+	return (
+		<footer className='footer'>
+			<div className='footer__inner'>
+				<p className='footer-text'>
+					2023. Банка полная любви. Все права защищены. Политика
+					конфиденциальности
+				</p>
+			</div>
+		</footer>
+	)
 }
 
 export default Footer
