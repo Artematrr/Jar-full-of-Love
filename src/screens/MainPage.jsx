38d839ec9fb0e55e799f50ffa7193ad008748353
Artem Bobylev <artematrr@gmail.com>
2024-03-01 13:30:20 +0400

    upd hero section

--- src/screens/MainPage.jsx
+++ src/screens/MainPage.jsx
@@ -1,10 +1,32 @@
 const MainPage = () => {
 	return (
 		<main className='main'>
-			<section className='banner container'>
-				<h2 className='banner__title'>
-					Оригинальный подарок для самого любимого, дорогого человека
-				</h2>
+			<section className='hero container'>
+				<div className='hero__info'>
+					<h2 className='hero__title'>
+						Оригинальный подарок для самого любимого, дорогого человека
+					</h2>
+					<h3 className='hero__title-red'>101 валентинка</h3>
+					<h4 className='hero__subtitle'>
+						Скажи все что хочешь сказать, о чем думаешь
+					</h4>
+					<ul className='hero__list'>
+						<li className='hero__list-item'>Ручная работа с любовью</li>
+						<li className='hero__list-item'>Безопасно и экологично</li>
+						<li className='hero__list-item'>Подарок к любому празднику</li>
+					</ul>
+					<div className='hero__buttons-container'>
+						<a href='#form' className='hero__button button button-fill'>
+							Заказать уже готовую
+						</a>
+						<button className='hero__button button button-outline'>
+							Собери свою банку
+						</button>
+					</div>
+				</div>
+				<div className='hero__image'>
+					<img src='img/hero-image.png' alt='Банка полная любви' />
+				</div>
 			</section>
 
 			<section className='catalog container' id='catalog'>
